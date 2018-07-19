5374025bd3cabd6d65bba538612c64b7572ae226
BogdanMateratskyy <bogdan.materatskyy@gmail.com>
2018-07-19 23:22:33 +0300

    Created parameterized class

---
+++ src/main/java/org/bm/exam/Exam.java
@@ -0,0 +1,31 @@
+package org.bm.exam;
+
+public class Exam<T extends Number> {
+	private T mark; // field parameter
+	private String name;
+
+	public Exam(T mark, String name) { // constructor parameter
+		this.mark = mark;
+		this.name = name;
+	}
+
+	public T getMark() { // method parameter
+		return mark;
+	}
+
+	public int roundMark() {
+		return Math.round(mark.floatValue()); //class Number method
+	}
+	
+	public boolean equalsToMark(Exam<T> obj){ // method parameter
+		return roundMark() == obj.roundMark();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
